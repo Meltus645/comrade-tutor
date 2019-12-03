6a22a602f01c55f97a0a4f72b6cb7ec13f2a2e40
Wa Juma <49098255+Meltus645@users.noreply.github.com>
2019-12-03 18:14:34 +0300

    Update Portfolio.js

--- Portfolio.js
+++ Portfolio.js
@@ -1,1 +1,22 @@
-﻿
+var contact=document.querySelector(".contact");
+var modal=document.querySelector(".contactmodal");
+var close=document.querySelector(".close");
+var open=document.querySelector(".quote");
+var video=document.querySelector(".video");
+
+(function(){
+   video.playbackRate=1.1;
+})();
+contact.onclick=function(){
+	modal.style.display="block";
+}
+
+
+open.onclick=function(){
+	modal.style.display="block";
+}
+
+close.onclick=function(){
+	modal.style.display="none";
+	document.body.style.opacity = 1;
+}
